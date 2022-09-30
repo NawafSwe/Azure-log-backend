a525f12b5539cfacc81977a7141d905d41ee876f
NawafSwe <nawaf.jr9@hotmail.com>
2022-10-01 01:57:00 +0300

    github actions

--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -1,12 +1,18 @@
 from flask import flash, render_template, redirect, request
 from FlaskExercise import app
-
-
+from datetime import datetime
 @app.route('/')
 def home():
     log = request.values.get('log_button')
-    # TODO: Appropriately log the different button presses
-    #   with the appropriate log level.
+    if log:
+        if log.lower()== 'info':
+         app.logger.info('%s button pressed at with value of %s', datetime.now(),log)
+        elif log.lower() == 'warning':
+            app.logger.warning('%s button pressed at with value of %s', datetime.now(),log)
+        elif log.lower() == 'error':
+         app.logger.error('%s button pressed at with value of %s', datetime.now(),log)
+        elif log.lower() == 'critical':
+         app.logger.critical('%s button pressed at with value of %s', datetime.now(),log)
     return render_template(
         'index.html',
         log=log
